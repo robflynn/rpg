cac747362e4ecbac661537720382ce5347e1e141
Rob Flynn <rob@thingerly.com>
2020-07-05 21:11:14 -0400

    Remove unneeded text.

--- src/lib/engine/scenes/map_scene.ts
+++ src/lib/engine/scenes/map_scene.ts
@@ -21,9 +21,6 @@ export default class MapScene extends Scene {
 
   render() {
     this.renderMap()
-
-    this.context.font = '18px Arial'
-    this.context.fillText("Hello, I am the map scene.", 100, 100)
   }
 
   private renderMap() {
